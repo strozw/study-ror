70c48c728bff132b9c6f5d632139e18c6c185c16
strozw <sxtxrxoxzxw@gmail.com>
2018-05-10 01:19:05 +0900

    :+1: Change test library to RSpec

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
-require_relative 'boot'
+# frozen_string_literal: true
 
-require 'rails/all'
+require_relative "boot"
+
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,7 +22,15 @@ module StudyRor
       g.stylesheets false
       g.javascripts false
       g.helper false
-      g.test_framework false
+      g.test_framework :rspec,
+        fixtures: true,
+        model_specs: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
 end
